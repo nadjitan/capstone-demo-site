42866f8ed72abc8ad57d445561e5081a6af02af9
nadjitan <nadjitan@yahooo.com.ph>
2022-07-02 20:52:04 +0800

    Replay system v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPageWithLayout = () => {
       <Head>
         <title>Home</title>
       </Head>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className=" text-3xl font-bold underline">Hello world!</h1>
     </>
   )
 }
--- pages/surveyor-client.tsx
+++ pages/surveyor-client.tsx
@@ -36,10 +36,12 @@ const SurveyorClient: NextPage = () => {
     let iframeDoc: Document
 
     iframe = document.getElementById("srvyr-website") as HTMLIFrameElement
+    const getIframeDoc = () => (iframeDoc = iframeDoc = iframe.contentDocument!)
 
     iframe.onload = () => {
-      iframeDoc = iframe.contentDocument!
+      getIframeDoc()
 
+      // RECORDING
       iframeDoc.addEventListener("click", e => {
         const elem = e.target! as HTMLElement
         const targetClass =
@@ -55,79 +57,49 @@ const SurveyorClient: NextPage = () => {
         }
       })
 
+      // REPLAY SYSTEM
       replayBtn!.onclick = () => {
         let prevEl: HTMLElement | null
 
         let arrIndex = 0
         let paused = false
+        prevEl = null
+        const activateElem = () => {
+          let elem: HTMLElement
+          iframeDoc.body.querySelectorAll("*").forEach(el => {
+            if (el.classList.contains(sampleClicks[arrIndex].class))
+              elem = el as HTMLElement
+          })
+
+          // TOGGLE STYLES
+          if (prevEl) prevEl.classList.remove("srvyr-selected")
+          elem!.classList.add("srvyr-selected")
+          if (elem!.tagName !== "A") elem!.click()
+
+          prevEl = elem!
+        }
+        // LOOPING CLASSES
         const clicksInterval = setInterval(() => {
           if (arrIndex < sampleClicks.length) {
             if (iframe.src !== sampleClicks[arrIndex].url) {
               paused = true
-              prevEl = null
+
               iframe.src = sampleClicks[arrIndex].url
               iframe.onload = e => {
-                const newIframe = e.target as HTMLIFrameElement
-                iframeDoc = newIframe.contentDocument!
+                getIframeDoc()
                 onClassChange(iframeDoc.body, () => {
                   paused = false
                 })
               }
             }
-
             if (paused === false) {
-              let elem: HTMLElement
-              iframeDoc.body.querySelectorAll("*").forEach(el => {
-                if (el.classList.contains(sampleClicks[arrIndex].class))
-                  elem = el as HTMLElement
-              })
-              if (prevEl) prevEl.classList.toggle("srvyr-selected")
-              elem!.classList.toggle("srvyr-selected")
-              prevEl = elem!
+              activateElem()
               arrIndex++
             }
           } else {
             clearInterval(clicksInterval)
           }
-        }, 600)
-
-        // sampleClicks.forEach((data, index) => {
-        //   if (iframe.src !== data.url) {
-        //     prevEl = null
-        //     iframe.src = data.url
-
-        //     iframe.onload = (e: Event) => {
-        //       setTimeout(() => {
-        //         const newIframe = e.target as HTMLIFrameElement
-        //         iframeDoc = newIframe.contentDocument!
-
-        //         onClassChange(iframeDoc.body, () => {
-        //           console.log("CLASS CHANGE!")
-        //           let elem: HTMLElement
-        //           iframeDoc.body.querySelectorAll("*").forEach(el => {
-        //             if (el.classList.contains(data.class))
-        //               elem = el as HTMLElement
-        //           })
-        //           if (prevEl) prevEl.classList.toggle("srvyr-selected")
-        //           elem!.classList.toggle("srvyr-selected")
-        //           prevEl = elem!
-        //         })
-        //       }, 0 + delay)
-        //       delay += 600
-        //     }
-        //   } else {
-        //     setTimeout(() => {
-        //       let elem: HTMLElement
-        //       iframeDoc.body.querySelectorAll("*").forEach(el => {
-        //         if (el.classList.contains(data.class)) elem = el as HTMLElement
-        //       })
-        //       if (prevEl) prevEl.classList.toggle("srvyr-selected")
-        //       elem!.classList.toggle("srvyr-selected")
-        //       prevEl = elem!
-        //     }, 0 + delay)
-        //     delay += 600
-        //   }
-        // })
+        }, 800)
       }
     }
   }, [])
