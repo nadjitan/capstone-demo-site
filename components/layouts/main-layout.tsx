6489f5989365bf9dda6123594599b26eb1ba042e
nadjitan <nadjitan@yahoo.com.ph>
2022-09-04 21:34:51 +0800

    Update package

--- components/layouts/main-layout.tsx
+++ components/layouts/main-layout.tsx
@@ -4,8 +4,7 @@ import { ComponentProps, ReactElement } from "react"
 
 import { Surveyor as Surveyortype } from "@nadjitan/surveyor"
 import "@nadjitan/surveyor/dist/style.css"
-import Navbar from "../navbar"
-
+import Navbar from "../Navbar"
 
 const Surveyor = dynamic<ComponentProps<typeof Surveyortype>>(
   () => import("@nadjitan/surveyor").then(mod => mod.Surveyor),
