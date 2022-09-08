ee33254ab211ede6f2d81c8047c42790da7ecf5c
nadjitan <nadjitan@yahoo.com.ph>
2022-09-08 12:29:29 +0800

    update package

--- components/layouts/main-layout.tsx
+++ components/layouts/main-layout.tsx
@@ -1,11 +1,10 @@
 import { NextPage } from "next"
 import dynamic from "next/dynamic"
-import { ComponentProps, ReactElement } from "react"
+import { ComponentProps, ReactElement, useEffect, useState } from "react"
 
 import { Surveyor as Surveyortype } from "@nadjitan/surveyor"
 import "@nadjitan/surveyor/dist/style.css"
 import Navbar from "../Navbar"
-
 
 const Surveyor = dynamic<ComponentProps<typeof Surveyortype>>(
   () => import("@nadjitan/surveyor").then(mod => mod.Surveyor),
@@ -13,11 +12,22 @@ const Surveyor = dynamic<ComponentProps<typeof Surveyortype>>(
 )
 
 const MainLayout: NextPage<{ children: ReactElement }> = ({ children }) => {
+  const [url, setUrl] = useState<string>()
+
+  useEffect(() => setUrl(window.location.href), [children])
+
   return (
     <Surveyor
-      lastData={{ url: "", class: "" }}
-      logClicks={false}
-      locateMsg={"Login page"}
+      lastData={{
+        url: "https://capstone-demo-site.vercel.app/login",
+        class: "srvyr-JvPMMKP4",
+      }}
+      logClicks={
+        url !== "https://capstone-demo-site.vercel.app/surveyor-client"
+          ? true
+          : false
+      }
+      locateMsg={"Sign in your account"}
       debug={true}
       apiUrl={"https://capstone-api-theta.vercel.app/api/telemetry"}>
       <Navbar />
